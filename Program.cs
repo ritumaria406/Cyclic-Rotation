96a0ea65015641eede2fe2c4f8dd6efc226a759d
Ritu Maria Joy <93349528+ritumaria406@users.noreply.github.com>
2021-11-03 22:26:48 +0530

    Cyclic Rotation
    Array - [1,2,3,5]
    K - 3
    
    Rotate by 3
      [1,2,3,5]
     1 -[2,3,5,1]
    2 - [3,5,1,2]
    3 - [5,1,2,3]
    Output - [5,1,2,3]

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        // Calling the Cyclic Rotation
+             CyclicRotation();
+        }
+
+        private static void CyclicRotation()
+        {
+            int[] C = Array.Empty<int>();
+            int[] A = { 1, 4, 5, 6 };
+            int K = Convert.ToInt32(Console.ReadLine());
+            foreach(int value in A)
+            {
+                Console.Write(" {0}",value);
+            }
+            
+            int[] B = new int[A.Length];
+
+            for(int i =0; i< A.Length; i++)
+            {
+                B[(i + K) % A.Length] = A[i];
+            }
+            foreach (int value in B)
+            {
+                Console.Write(" {0}", value);
+            }
+        }
+     }
+}
